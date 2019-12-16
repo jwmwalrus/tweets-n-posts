7cbc34acdf4b74b4ccc875d5ad61b6998db162b8
JWM <jwmwalrus@gmail.com>
2019-12-16 09:34:22 -0500

    Add linters

---
+++ config/object-manager.php
@@ -0,0 +1,10 @@
+<?php
+
+use App\Kernel;
+
+require_once dirname(__DIR__) . '/../config/bootstrap.php';
+
+$kernel = new Kernel($_SERVER['APP_ENV'], (bool) $_SERVER['APP_DEBUG']);
+$kernel->boot();
+
+return $kernel->getContainer()->get('doctrine')->getManager();
