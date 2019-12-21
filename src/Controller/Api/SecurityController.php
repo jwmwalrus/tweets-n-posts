2a44de6f5a1a372e31e4a491f9d847dde0c561d7
JWM <jwmwalrus@gmail.com>
2019-12-21 10:46:24 -0500

    Add exceptions to the register endpoint

--- src/Controller/Api/SecurityController.php
+++ src/Controller/Api/SecurityController.php
@@ -3,12 +3,15 @@
 namespace App\Controller\Api;
 
 use App\Entity\User;
+use Doctrine\DBAL\Exception\UniqueConstraintViolationException;
 use Doctrine\ORM\EntityManagerInterface;
 use FOS\RestBundle\Controller\AbstractFOSRestController;
 use FOS\RestBundle\Controller\Annotations\RequestParam;
 use FOS\RestBundle\Request\ParamFetcher;
 use FOS\RestBundle\View\View;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\Exception\BadRequestHttpException;
+use Symfony\Component\HttpKernel\Exception\ConflictHttpException;
 
 class SecurityController extends AbstractFOSRestController
 {
@@ -56,36 +59,44 @@ class SecurityController extends AbstractFOSRestController
 
         $user = new User();
 
-        foreach ($paramfetcher->all() as $k => $v) {
-            switch ($k) {
-                case 'name':
-                    $user->setName($v);
-                    break;
-                case 'username':
-                    $user->setUsername($v);
-                    break;
-                case 'email':
-                    $user->setEmail($v);
-                    break;
-                case 'password':
-                    $password = password_hash(
-                        $v,
-                        PASSWORD_BCRYPT,
-                        [
-                            'cost' => 14,
-                        ]
-                    );
-                    $user->setPassword($password);
-                    break;
-                case 'twitterid':
-                    $user->setTwitterid($v);
-                    break;
-                default:
+        try {
+            foreach ($paramfetcher->all() as $k => $v) {
+                switch ($k) {
+                    case 'name':
+                        $user->setName($v);
+                        break;
+                    case 'username':
+                        $user->setUsername($v);
+                        break;
+                    case 'email':
+                        $user->setEmail($v);
+                        break;
+                    case 'password':
+                        $password = password_hash(
+                            $v,
+                            PASSWORD_BCRYPT,
+                            [
+                                'cost' => 14,
+                            ]
+                        );
+                        $user->setPassword($password);
+                        break;
+                    case 'twitterid':
+                        $user->setTwitterid($v);
+                        break;
+                    default:
+                }
             }
-        }
 
-        $em->persist($user);
-        $em->flush();
+            $em->persist($user);
+            $em->flush();
+        } catch (UniqueConstraintViolationException $e) {
+            throw new ConflictHttpException('Username is already taken');
+        } catch (DBALException $e) {
+            throw new BadRequestHttpException('Error executing SQL statements: ' + $e->getMessage());
+        } catch (\Throwable $e) {
+            throw new BadRequestHttpException($e->getMessage());
+        }
 
         $data = [
             'id' => $user->getId(),
